45fed003940c81a665a6064ecfa8d9df710f93cf
dev.java.works@gmail.com <dev.java.works@gmail.com>
2019-02-25 00:03:17 +0530

    First Commit

---
+++ src/main/java/com/ssstsar/soap/webservices/soapcoursemanagement/SoapCourseManagementApplication.java
@@ -0,0 +1,12 @@
+package com.ssstsar.soap.webservices.soapcoursemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapCourseManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapCourseManagementApplication.class, args);
+	}
+}
