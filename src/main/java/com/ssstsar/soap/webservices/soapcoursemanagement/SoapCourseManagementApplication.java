7a3dbf5342f2e69df91a95dbf7137492a26fbf8f
dev.java.works@gmail.com <dev.java.works@gmail.com>
2019-03-15 08:06:03 +0530

    eureka

--- src/main/java/com/ssstsar/soap/webservices/soapcoursemanagement/SoapCourseManagementApplication.java
+++ src/main/java/com/ssstsar/soap/webservices/soapcoursemanagement/SoapCourseManagementApplication.java
@@ -2,7 +2,9 @@ package com.ssstsar.soap.webservices.soapcoursemanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class SoapCourseManagementApplication {
 
